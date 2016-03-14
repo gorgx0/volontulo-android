03c2399e03ada0e48b9816eb10cdae7e2161a967
Radosław Drewa <radoslaw.drewa@stxnext.pl>
2016-03-14 09:59:27 +0100

    Flow between activities, Login activity/layout

--- Volontulo/app/src/main/java/com/stxnext/volontulo/MainActivity.java
+++ Volontulo/app/src/main/java/com/stxnext/volontulo/MainActivity.java
@@ -11,23 +11,28 @@ public class MainActivity extends VolontuloBaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init(R.string.app_name);
     }
 
+    @SuppressWarnings("unused")
     @OnClick(R.id.button_action_list)
     public void gotoActionList() {
         Toast.makeText(this, "ACTION LIST", Toast.LENGTH_SHORT).show();
     }
 
+    @SuppressWarnings("unused")
     @OnClick(R.id.button_volunteer_list)
     public void gotoVolunteerList() {
         Toast.makeText(this, "VOLUNTEER LIST", Toast.LENGTH_SHORT).show();
     }
 
+    @SuppressWarnings("unused")
     @OnClick(R.id.button_communicator)
     public void gotoCommunicator() {
         Toast.makeText(this, "COMMUNICATOR", Toast.LENGTH_SHORT).show();
     }
 
+    @SuppressWarnings("unused")
     @OnClick(R.id.button_settings)
     public void gotoSettings() {
         Toast.makeText(this, "SETTINGS", Toast.LENGTH_SHORT).show();
--- Volontulo/app/src/main/java/com/stxnext/volontulo/VolontuloBaseActivity.java
+++ Volontulo/app/src/main/java/com/stxnext/volontulo/VolontuloBaseActivity.java
@@ -1,11 +1,34 @@
 package com.stxnext.volontulo;
 
+import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import butterknife.ButterKnife;
 
 public abstract class VolontuloBaseActivity extends AppCompatActivity {
+
+    protected Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    protected void init() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+    }
+
+    protected void init(int resourceTitle) {
+        init();
+        toolbar.setTitle(resourceTitle);
+    }
+
+    protected void init(String stringTitle) {
+        init();
+        toolbar.setTitle(stringTitle);
+    }
 
     @Override
     public void setContentView(@LayoutRes int layoutResID) {
