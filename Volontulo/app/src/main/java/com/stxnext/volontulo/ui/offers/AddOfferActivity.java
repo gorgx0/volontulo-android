1efd2ce7d5c61c4144d02a7a66298130fe02cb35
Mateusz Kaźmierczak <mateusz.kazmierczak@stxnext.pl>
2016-03-15 11:21:47 +0100

    Adds skeleton screen for adding offer

---
+++ Volontulo/app/src/main/java/com/stxnext/volontulo/ui/offers/AddOfferActivity.java
@@ -0,0 +1,15 @@
+package com.stxnext.volontulo.ui.offers;
+
+import android.os.Bundle;
+
+import com.stxnext.volontulo.R;
+import com.stxnext.volontulo.VolontuloBaseActivity;
+
+public class AddOfferActivity extends VolontuloBaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offer_add);
+        init("Dodaj ofertę");
+    }
+}
