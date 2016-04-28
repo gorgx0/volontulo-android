3ddcbca5e8075aa92c5e6be5f8c74b36aa6cfb90
Mateusz Kaźmierczak <mateusz.kazmierczak@stxnext.pl>
2016-04-28 15:41:19 +0200

    Adds send message to selected user

--- Volontulo/app/src/main/java/com/stxnext/volontulo/ui/volunteers/AttendHeaderViewHolder.java
+++ Volontulo/app/src/main/java/com/stxnext/volontulo/ui/volunteers/AttendHeaderViewHolder.java
@@ -1,15 +1,24 @@
 package com.stxnext.volontulo.ui.volunteers;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 import com.stxnext.volontulo.R;
+import com.stxnext.volontulo.api.User;
 import com.stxnext.volontulo.api.UserProfile;
+import com.stxnext.volontulo.logic.im.Conversation;
 import com.stxnext.volontulo.model.Ofer;
+import com.stxnext.volontulo.ui.im.MessagesListFragment;
+import com.stxnext.volontulo.ui.im.MessagingActivity;
+
+import org.parceler.Parcels;
 
 import butterknife.Bind;
+import butterknife.OnClick;
 
 /**
  * Created by rdrewa on 14.04.16.
@@ -54,5 +63,15 @@ public class AttendHeaderViewHolder extends AttendViewHolder {
                     .placeholder(R.drawable.ic_user_placeholder)
                     .into(image);
         }
+    }
+
+    @OnClick(R.id.send_message_to)
+    void onClick(View clicked) {
+        final Context context = clicked.getContext();
+        final User user = userProfile.getUser();
+        final Conversation conversation = Conversation.createOrUpdate(context, user);
+        final Intent start = new Intent(context, MessagingActivity.class);
+        start.putExtra(MessagesListFragment.KEY_PARTICIPANTS, Parcels.wrap(conversation));
+        context.startActivity(start);
     }
 }
